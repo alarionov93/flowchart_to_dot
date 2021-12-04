29bed2891cf7d42d7508630f71a17b7157dcc67e
AnastasiyaMushta <anastasiya_mushta@mail.ru>
2021-12-04 14:55:04 +0500

    fixed errors with def finding

--- get_all_func.py
+++ get_all_func.py
@@ -1,20 +1,28 @@
 import glob
+import sys
 
-STATE=0
-for f in glob.glob('*.py'):
+STATE = 0
+path = sys.argv[1]
+for f in glob.glob('%s*.py' % path):
+    if f != last_f:
+        STATE = 0
     for l in open(f, 'r').readlines():
+        func_name = re.findall(r'^def\s(\w+)', l)
         if STATE == 0: #init
-            if 'def ' in l:
-                w = open('descr/%s.py' % l[4:6].strip(), 'a')
+            if func_name:
+                w = open('%sdescr/%s.py' % (path, func_name), 'a')
                 w.write(l)
                 STATE = 1
         elif STATE == 1:
             try:
-                if 'def ' in l:
+                if func_name:
                     w.close()
-                    w = open('descr/%s.py' % l[4:6].strip(), 'a')
+                    w = open('%sdescr/%s.py' % (path, func_name), 'a')
                     w.write(l)
                 else:
                     w.write(l)
             except NameError:
                 pass
+    last_f = f
+
+ 
